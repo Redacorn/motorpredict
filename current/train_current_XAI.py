5fc04dab3d371b71fe9b6d2039d3dfd631ee089e
redacorn <asqer92@hanyang.ac.kr>
2023-12-13 23:47:55 +0900

    mp

--- current/train_current_XAI.py
+++ current/train_current_XAI.py
@@ -162,17 +162,13 @@ def main():
     df_list, x_list, y_list = [], [], []
 
     for i in tqdm(range(5)):
-        df = pd.read_csv(data_path + f'/train_df_{i}.csv')
-        df_list.append(df.iloc[:, 1:])
-        # x_list.append(df.drop(['state'], axis=1).drop(['Unnamed: 0'], axis=1))
-        # y_list.append(df['state'])
+        tasks = [(df_list[i], save_path, i, result_path) for i in range(5)]
 
-    # train model
-    model_0 = train(df_list[0], save_path, 0, result_path)
-    model_1 = train(df_list[1], save_path, 1, result_path)
-    model_2 = train(df_list[2], save_path, 2, result_path)
-    model_3 = train(df_list[3], save_path, 3, result_path)
-    model_4 = train(df_list[4], save_path, 4, result_path)
+        # train model
+        with multiprocessing.Pool() as pool:
+            models = pool.map(worker, tasks)
+
+        model_0, model_1, model_2, model_3, model_4 = models
 
 
 if __name__ == "__main__":
